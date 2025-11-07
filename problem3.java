b7ad35649f8de25ca215894a9a9be77331ea8ca4
Shakthivel <shakthivelsachin56@gmail.com>
2025-11-07 20:54:55 +0530

    Roman to Integer

---
+++ problem3.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class problem33{
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);    map.put('V', 5);
+        map.put('X', 10);   map.put('L', 50);
+        map.put('C', 100);  map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int curr = map.get(s.charAt(i));
+            int next = (i + 1 < s.length()) ? map.get(s.charAt(i + 1)) : 0;
+
+            if (curr < next) {
+                result -= curr;
+            } else {
+                result += curr;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String roman = s.nextLine();
+        Solution sol = new Solution();
+        int value = sol.romanToInt(roman);
+        System.out.println("Output: " + value);
+    }
+}
