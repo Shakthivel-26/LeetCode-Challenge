50f88700e48a4efcbbc37cc8812688f4f2c2dddf
Shakthivel <shakthivelsachin56@gmail.com>
2025-11-07 20:58:13 +0530

    Longest Common Prefix

---
+++ problem4.java
@@ -0,0 +1,21 @@
+class problem4{
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.longestCommonPrefix(new String[]{"flower","flow","flight"})); // Output: fl
+        System.out.println(sol.longestCommonPrefix(new String[]{"dog","racecar","car"}));   // Output: 
+    }
+}
