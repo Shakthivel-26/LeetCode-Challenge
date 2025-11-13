effef0e6261a723667e9c6ec9664188e0dc4215a
Shakthivel <shakthivelsachin56@gmail.com>
2025-11-13 14:09:55 +0530

    Search Insert position

---
+++ problem9.java
@@ -0,0 +1,21 @@
+public class problem9{
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;   
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return left; // insertion position
+    }
+    public static void main(String[] args) {
+        problem9 object = new problem9();
+        System.out.println(object.searchInsert(new int[]{1,3,5,6}, 5)); // 2
+        System.out.println(object.searchInsert(new int[]{1,3,5,6}, 2)); // 1
+        System.out.println(object.searchInsert(new int[]{1,3,5,6}, 7)); // 4
+    }
+}
