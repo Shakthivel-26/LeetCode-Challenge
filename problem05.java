728b89d1b1089423b98f606d9f1716b263d76743
Shakthivel <shakthivelsachin56@gmail.com>
2025-12-11 22:38:14 +0530

    create problem05.java

--- problem05.java
+++ problem05.java
@@ -2,17 +2,14 @@ import java.util.*;
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
         for (char c : s.toCharArray()) {
             if (c == '(') stack.push(')');
             else if (c == '{') stack.push('}');
             else if (c == '[') stack.push(']');
             else if (stack.isEmpty() || stack.pop() != c) return false;
         }
-
         return stack.isEmpty();
     }
-
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(sol.isValid("()"));      // true
